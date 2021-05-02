8f5253c278e9cf7e374678f77086ed1f5f297699
bfly2001 <brendan.sflynn@gmail.com>
2021-05-02 08:24:02 -0700

    added drag and drop attributes to elements/ added javascript for drag and drop. removed unused css.
    added drag and drop attributes to elements/ added javascript for drag and drop. removed unused css.

---
+++ script.js
@@ -0,0 +1,14 @@
+function allowDropThis(i) {
+    i.preventDefault();
+}
+
+function dragThis(i) {
+    i.dataTransfer.setData("checker", i.target.id);
+}
+
+function dropThis(i) {
+    i.preventDefault();
+    var data = i.dataTransfer.getData("checker");
+    i.target.appendChild(document.getElementById(data));
+}
+
