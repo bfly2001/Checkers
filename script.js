c3d22d20cb39e11ad41de605f007ab2ddd96b742
bfly2001 <brendan.sflynn@gmail.com>
2021-05-23 10:09:36 -0700

    Update script.js
    rewrote all drag functions.

--- script.js
+++ script.js
@@ -55,25 +55,42 @@ function dropThis(i) {
     i.target.append(document.getElementById(data));
 }*/
 
+'use strict';
+
 function dragStart(e) {
-    e.dataTransfer.effectAllowed="move";
-    e.dataTransfer.setData('Text', e.target.getAttribute('id'));
-    e.dataTransfer.setDragImage(e.target, 0,0);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData('Text', e.target.id);
+    //e.dataTransfer.setDragImage(e.target, 0,0);
+    console.log('start');
     return true;
+
 }
 
 function dragEnter(e) {
-    event.preventDefault();
-    return true;
+    e.preventDefault();
+    if(e.target.hasChildNodes() == true) {
+        e.target.firstChild.remove();
+    } else if (e.target.hasChildNodes() == false) {
+       //do nothing
+       return false; 
+    }
 }
 
 function dragOver(e) {
+    e.preventDefault();
     return false;
 }
 
+function dragLeave(e) {
+    e.preventDefault();
+    return false;
+}
+
+
 function dragDrop(e) {
     let src = e.dataTransfer.getData('Text');
-    e.target.appendChild(document.getElementById(src));
+    e.target.append(document.getElementById(src));
     e.stopPropagation();
+    console.log('drop');
     return false;
 }
